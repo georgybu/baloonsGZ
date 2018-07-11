ae8b45649ab16a7642d4ca44580c36ebec3f5674
Georgy Bunin <georgyb@mprest.com>
2018-07-11 21:15:11 +0300

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CapturePageComponent } from './pages/capture-page/capture-page.component';
+import { MapPageComponent } from './pages/map-page/map-page.component';
+import { CommentPageComponent } from './pages/comment-page/comment-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'capture', component: CapturePageComponent},
+  {path: 'map', component: MapPageComponent},
+  {path: 'comment', component: CommentPageComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CapturePageComponent } from './pages/capture-page/capture-page.component';
+import { MapPageComponent } from './pages/map-page/map-page.component';
+import { CommentPageComponent } from './pages/comment-page/comment-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CapturePageComponent,
+    MapPageComponent,
+    CommentPageComponent
   ],
   imports: [
     BrowserModule,
